8749c8a8f596240373715e2b81ed8f52bec50db0
Timo-joinllur <111872301+Timo-joinllur@users.noreply.github.com>
2023-02-07 10:25:52 +0200

    task ex1

---
+++ web programming/web-dev-env-main/src/ex1.php
@@ -0,0 +1,39 @@
+<?php $title = "PHP is interesting";
+include "header.php" ?>
+<?php 
+//opening tag
+    echo "Timofei Beresnev";
+    echo " BBCAP22A3";
+    echo "<br> Hello world! My name is David <br>";
+    echo "Today is " . date("Y/m/d") . "<br>";
+    
+?>
+<?php $g1 = 5; $g2=4; $g3=5;
+echo "<table>
+<tr>
+    <td>S.N</td>
+    <td>Name</td>
+    <td>Grade</td>
+</tr>
+<tr>
+    <td>1</td>
+    <td>Pekka</td>
+    <td>5</td>
+</tr>
+<tr>
+    <td>2</td>
+    <td>Johanna</td>
+    <td>4</td>
+</tr>
+<tr>
+    <td>3</td>
+    <td>John</td>
+    <td>5</td>
+</tr>
+</table>        
+"
+?>
+<img src= ' images/screenshot.png ' width="500" height="228" > ;
+
+<?php include "footer.php" ?>
+
